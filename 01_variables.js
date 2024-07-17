268efd7fb0592ba5b691c0d23a6fb2f13ed8bc5d
ajay <ajaykumar290904@gmail.com>
2024-07-17 20:06:27 +0530

    Learn variabled and datatypes

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 123456
+let accountEmail = "hjfdh@gamil.com"
+var accountPassword = "545t53t4"
+accountCity = "Noida"
+let accountState;
+
+// accountId = 23 // const keyword does not change 
+console.log(accountId);
+
+/*
+Prefer not to ise var 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState]) //this method give mutiple output in table forms
---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+"Use strict"; //trest all js code as newer version 
+
+
+
+let name = "Ajay"
+let age = 20
+let isLoggedIn = false
+let state = null
+
+//number => 2 to power 52
+// bigint
+//string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined
+
+// symbol => unique
+
+// object
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
