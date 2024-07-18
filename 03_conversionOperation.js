77688a0c6f7d29dde7e91fddbdf81a685af3c861
ajay <ajaykumar290904@gmail.com>
2024-07-18 18:56:58 +0530

    conversion of data type

---
+++ 03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = 33  // true/false , "33" , "33abc" , ajay
+
+console.log(typeof score);
+console.log(typeof (score))
+
+
+let valueInNumber = Number(score) // convert the value
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+let isLoggedIn = 1
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 34
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);  // it give the output and the seems like to be a number but it is a string you can print its typeof it give the outout is string 
+console.log(typeof stringNumber);
+
