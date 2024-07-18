4548ea8016d838da672b67b31ccaad8cd8493fc2
ajay <ajaykumar290904@gmail.com>
2024-07-18 19:28:13 +0530

    conversion of data type

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,19 +1,19 @@
 let score = 33  // true/false , "33" , "33abc" , ajay
 
-console.log(typeof score);
-console.log(typeof (score))
+//console.log(typeof score);
+//console.log(typeof (score))
 
 
 let valueInNumber = Number(score) // convert the value
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
 
 
 let isLoggedIn = 1
 
 let booleanIsloggedIn = Boolean(isLoggedIn)
-console.log(booleanIsloggedIn);
+//console.log(booleanIsloggedIn);
 
 // 1 => true ; 0 => false
 // "" => false
@@ -23,6 +23,46 @@ console.log(booleanIsloggedIn);
 let someNumber = 34
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);  // it give the output and the seems like to be a number but it is a string you can print its typeof it give the outout is string 
-console.log(typeof stringNumber);
 
+//console.log(stringNumber);  
+// it give the output and the seems like to be a number but it is a string you can print its typeof it give the outout is string 
+
+//console.log(typeof stringNumber);
+
+
+
+
+// *********************** OPERATION **********************
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Ajay"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+
+
+let gameCounter = 100;
+++gameCounter;
+//gameCounter++;
+console.log(gameCounter);
