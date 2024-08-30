bdd1c7cd6185312f8b899355d9a10f7883e48584
ajay <ajaykumar290904@gmail.com>
2024-08-30 18:32:09 +0530

    learn how js executes

---
+++ callstack.js
@@ -0,0 +1,15 @@
+
+
+let val1 = 12
+let val2 = 2
+
+function addNum (num1 , num2)
+{
+    let total = num1 + num2
+    return total
+}
+
+let result1 = addNum(val1 , val2)
+let result2 = addNum(3,4)
+
+
