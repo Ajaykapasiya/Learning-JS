722fee6783c4770dd80de29fbabb517098e6fd37
ajay <ajaykumar290904@gmail.com>
2024-08-30 22:34:58 +0530

    controlflow

---
+++ controlflow.js
@@ -0,0 +1,50 @@
+// if 
+
+const userLoggedIn = true
+
+if (2==2) {
+    console.log("true");
+    
+}
+
+
+const temp = 42
+
+if (temp<50) {
+    console.log("less the 50");
+    
+} else {
+    console.log("not less then 50");
+    
+}
+
+
+const bal =  1000
+if(bal>200) console.log("yess");
+
+// switch
+const month = 3
+
+switch (month) {
+    case 1:
+        
+     console.log("Jan");
+     
+
+        break;
+    case 2:
+        
+     console.log("Feb");
+     
+
+        break;
+    case 3:
+        
+     console.log("Mar");
+     
+
+        break;
+
+    default:
+        break;
+}
