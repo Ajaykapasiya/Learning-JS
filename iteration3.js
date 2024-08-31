14d53cda3ce32b7e661cf9edd3721d9eeb17b212
ajay <ajaykumar290904@gmail.com>
2024-08-31 23:13:16 +0530

    learn map

---
+++ iteration3.js
@@ -0,0 +1,30 @@
+//forof
+
+const arr =[ 1,2,3,4,5]
+
+for (const num of arr) {
+    console.log(num);
+    
+} 
+
+
+const greeting = "Hello world"
+
+for (const greet of greeting) {
+    console.log(`Each char is ${greet}`);
+    
+    
+}
+
+
+
+// Map
+
+const map = new Map()
+
+map.set('IN', "India")
+map.set('USA',"United states of america")
+
+
+console.log(map);
+
