852f5b96f8843ef5b9110f8862487b36e62e78a1
ajay <ajaykumar290904@gmail.com>
2024-09-02 18:20:06 +0530

    Learn filter and reduce

--- filtermapandreduce2.js
+++ filtermapandreduce2.js
@@ -20,3 +20,53 @@ const newNumss = myNums
                 .filter((nums) => nums > 40)
 
 console.log(newNumss);
+
+
+
+// Reduce
+
+const numrs = [1,2,3]
+
+//const myTotal = numrs.reduce(function (acc , currval)
+//{
+//    console.log(`acc value ${acc} and current value ${currval}`);
+//    
+//    return acc + currval
+//}, 0)
+
+const myTotal = numrs.reduce((acc , currval ) => acc+currval , 0)
+ 
+console.log(myTotal);
+
+
+const course = [
+    {
+        itemName : "js course",
+        price : 1000
+    },
+
+    {
+        itemName : "c course",
+        price : 1230
+    },
+
+    {
+        itemName : "java course",
+        price : 9876
+    },
+
+    {
+        itemName : "cpp course",
+        price : 3443
+    },
+
+    {
+        itemName : "ruby course",
+        price : 14343
+    },
+]
+
+
+const finalPrice = course.reduce((acc , course) => acc+course.price , 0  )
+
+console.log(`the total price of course ${finalPrice}`);
