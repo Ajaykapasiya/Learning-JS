da423f0571a82aab13a29996b871eed02e00089b
ajay <ajaykumar290904@gmail.com>
2025-01-31 23:26:44 +0530

    add: populate book data in fitermapandreduce.js

--- fitermapandreduce.js
+++ fitermapandreduce.js
@@ -10,7 +10,16 @@
 
 
 const books = [
-    
+    {title: ' Book one ' , genre : 'history' , publish: 2001, edition: 2024} ,  
+    {title: ' Book two ' , genre : 'science' , publish: 1289, edition: 1390} ,
+    {title: ' Book three ' , genre : 'physics',publish: 2002, edition: 2010} ,
+    {title: ' Book four ' , genre : 'maths' ,  publish: 2001, edition: 2020} ,
+    {title: ' Book five ' , genre : 'history' ,publish: 2001, edition: 2024} ,
+    {title: ' Book six ' , genre : 'chemistry',publish: 2004, edition: 2023} ,
+    {title: ' Book seven ' , genre : 'history',publish: 2001, edition: 2024} ,
+    {title: ' Book eight ' , genre : 'maths' , publish: 2005, edition: 2024} ,
+    {title: ' Book nine ' , genre : 'physics' ,publish: 2019, edition: 2024} ,
+
 ];
 
 
