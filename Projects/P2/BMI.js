4868183a904e3b675759e0701f1c25e8b0fc6da1
ajay <ajaykumar290904@gmail.com>
2025-02-04 00:24:43 +0530

    add: enhance BMI calculation results with weight categories and update HTML guide

--- Projects/P2/BMI.js
+++ Projects/P2/BMI.js
@@ -5,7 +5,6 @@ form.addEventListener("submit", function (e) {
   const height = parseInt(document.querySelector("#height").value);
   const weight = parseInt(document.querySelector("#weight").value);
   const results = document.querySelector("#results");
-
   if (height === "" || height < 0 || isNaN(height)) {
     results.innerHTML = "Please give a valid height";
   } else if (weight === "" || weight < 0 || isNaN(weight)) {
@@ -13,9 +12,21 @@ form.addEventListener("submit", function (e) {
   }
   else{
    const bmi =  (weight/((height*height)/10000)).toFixed(2)
+   console.log(bmi);
    
 
-   results.innerHTML = `<span>${bmi}</span>`
+   results.innerHTML = `<span>BMI =  ${bmi}</span>`
+
+   if (bmi < 18.5) {
+  results.innerHTML = `${bmi} , Under Weight`
+   }
+
+   else if (bmi >= 18.6 && bmi < 24.9 ) {
+ results.innerHTML = `${bmi} , Normal range`
+   }
+   else if (bmi >= 25 ){
+  results.innerHTML = `${bmi} , Over weight `
+   }
    
   }
 });
