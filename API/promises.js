e07d0b8dd358f38bd0e1090fb59917b0dfa288c5
ajay <ajaykumar290904@gmail.com>
2025-02-07 12:25:32 +0530

    add: implement additional promise examples in promises.js

--- API/promises.js
+++ API/promises.js
@@ -5,19 +5,31 @@ const promises = new Promise(function (resolve, reject) {
   }, 1000);
 });
 
-promises.then(function(){
-    console.log('I am done');
-    
-})
+promises.then(function () {
+  console.log("I am done");
+});
 
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Promise is done 2");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("I am done 2");
+});
 
+const promiseThree = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve({ username: "ajay", email: "ajay@gmail.com" });
+  }, 1000);
+});
 
-new Promise (function (resolve ,reject){
+promiseThree.then(function (user) {
+  console.log(user);
+});
+
+const promiseFour = new Promise(function(resolve , reject){
     setTimeout(function(){
-       console.log("Promise is done 2");
-        resolve()
-    },1000)
-}).then(function(){
-    console.log("I am done 2");
-    
+
+    } , 1000)
 })
