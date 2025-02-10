afb3319ceb74cea549b26dc0fb682e08ad6d3f2f
ajay <ajaykumar290904@gmail.com>
2025-02-10 21:53:22 +0530

    refactor: remove unused promise implementation in promises.js

--- API/promises.js
+++ API/promises.js
@@ -1,11 +1,3 @@
-const { response } = require("express");
-
-const promises = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    console.log("Promise is done");
-    resolve();
-  }, 1000);
-});
 
 promises.then(function () {
   console.log("I am done");
