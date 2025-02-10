e016690fe4bed9151db15917f327eb74d575626d
ajay <ajaykumar290904@gmail.com>
2025-02-10 21:53:36 +0530

    add: implement a new promise in promises.js to log completion after a timeout

--- API/promises.js
+++ API/promises.js
@@ -1,3 +1,11 @@
+const { response } = require("express");
+
+const promises = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Promise is done");
+    resolve();
+  }, 1000);
+});
 
 promises.then(function () {
   console.log("I am done");
