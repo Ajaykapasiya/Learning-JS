30bbece6d8f0f40bc3649b3c720c6553230a6c09
ajay <ajaykumar290904@gmail.com>
2025-02-12 20:24:14 +0530

    refactor: remove newGame function from guess.js

--- Projects/P4/guess.js
+++ Projects/P4/guess.js
@@ -76,18 +76,4 @@ function endGame() {
   newGame();
 }
 
-function newGame() {
-  const gameNewButton = document.querySelector("#newGame");
-  gameNewButton.addEventListener("click", function (e) {
-    randomNumber = parseInt(Math.random() * 100 + 1);
-    prevGuess = [];
-    numGuess = 1;
-    guessSlot.innerHTML = '';
-    remaining.innerHTML = `${11 - numGuess}`;
-    userInput.removeAttribute('disabled')
-    startOver.removeChild(p);
-
-
-    playGame = true;
-  });
-}
+f
