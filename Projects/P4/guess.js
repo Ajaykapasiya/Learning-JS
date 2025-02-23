9eb973661d49e417f1897849395f7a996bc547a5
ajay <ajaykumar290904@gmail.com>
2025-02-23 22:43:35 +0530

    refactor: remove unused displayGuess function from guess.js

--- Projects/P4/guess.js
+++ Projects/P4/guess.js
@@ -55,12 +55,6 @@ function checkGuess(guess) {
   }
 }
 
-function displayGuess(guess) {
-  userInput.value = "";
-  guessSlot.innerHTML += `${guess}  `;
-  numGuess++;
-  remaining.innerHTML = `${11 - numGuess}`;
-}
 
 function displayMessage(message) {
   lowOrHi.innerHTML = `<h2>${message}</h2>`;
